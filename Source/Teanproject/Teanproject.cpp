bd69adc7b6561019438cfb11c8f51b8c66bb5093
JJ00ng <whdgur0316@Naver.com>
2025-08-28 19:41:45 +0900

    새 프로젝트 생성
    언리얼 프로젝트의 생성.
    소스트리를 활용한 커밋과 푸시 방법 학습.
    커밋 메세지와 제목과 내용은 한 줄 띄기를 통해 구분한다는 것을 알게됨.

---
+++ Source/Teanproject/Teanproject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Teanproject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Teanproject, "Teanproject" );
